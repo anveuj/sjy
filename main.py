a4b5b167381ec2bf2f850a5b6bce4f5a7f1e342e
anveuj <114328947+anveuj@users.noreply.github.com>
2025-02-12 17:25:16 +0800

    Update main.py

--- main.py
+++ main.py
@@ -14,30 +14,44 @@ def get_color():
     return random.choice(color_list)
 
 
-def get_access_token():
+def get_config():
+    try:
+        with open("config.txt", encoding="utf-8") as f:
+            return eval(f.read())
+    except FileNotFoundError:
+        print("推送消息失败，请检查config.txt文件是否与程序位于同一路径")
+        os.system("pause")
+        sys.exit(1)
+    except SyntaxError:
+        print("推送消息失败，请检查配置文件格式是否正确")
+        os.system("pause")
+        sys.exit(1)
+
+
+def get_access_token(config):
     # appId
     app_id = config["app_id"]
     # appSecret
     app_secret = config["app_secret"]
-    post_url = ("https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid={}&secret={}"
-                .format(app_id, app_secret))
+    post_url = (
+        f"https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid={app_id}&secret={app_secret}"
+    )
     try:
         access_token = get(post_url).json()['access_token']
     except KeyError:
         print("获取access_token失败，请检查app_id和app_secret是否正确")
         os.system("pause")
         sys.exit(1)
-    # print(access_token)
     return access_token
 
 
-def get_weather(region):
+def get_weather(region, config):
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
                       'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'
     }
     key = config["weather_key"]
-    region_url = "https://geoapi.qweather.com/v2/city/lookup?location={}&key={}".format(region, key)
+    region_url = f"https://geoapi.qweather.com/v2/city/lookup?location={region}&key={key}"
     response = get(region_url, headers=headers).json()
     if response["code"] == "404":
         print("推送消息失败，请检查地区名是否有误！")
@@ -50,7 +64,7 @@ def get_weather(region):
     else:
         # 获取地区的location--id
         location_id = response["location"][0]["id"]
-    weather_url = "https://devapi.qweather.com/v7/weather/now?location={}&key={}".format(location_id, key)
+    weather_url = f"https://devapi.qweather.com/v7/weather/now?location={location_id}&key={key}"
     response = get(weather_url, headers=headers).json()
     if "now" in response:
         now_data = response["now"]
@@ -62,10 +76,16 @@ def get_weather(region):
             temp += u"\N{DEGREE SIGN}" + "C"
         # 风向
         wind_dir = now_data.get("windDir")
-        return weather, temp, wind_dir
+        # 空气湿度、空气质量、PM2.5、紫外线等信息可以根据接口文档添加获取逻辑
+        # 这里假设接口返回的数据中有这些字段
+        air_humidity = now_data.get("humidity", "暂无数据")
+        air_quality = now_data.get("airQuality", "暂无数据")
+        pm = now_data.get("pm2p5", "暂无数据")
+        ultraviolet_rays = now_data.get("uvIndex", "暂无数据")
+        return weather, temp, wind_dir, air_humidity, air_quality, pm, ultraviolet_rays
     else:
         print("天气接口响应数据中不存在 'now' 键:", response)
-        return None, None, None
+        return None, None, None, None, None, None, None
 
 
 def get_birthday(birthday, year, today):
@@ -85,7 +105,6 @@ def get_birthday(birthday, year, today):
         birthday_day = birthday.day
         # 今年生日
         year_date = date(year, birthday_month, birthday_day)
-
     else:
         # 获取国历生日的今年对应月和日
         birthday_month = int(birthday.split("-")[1])
@@ -122,8 +141,8 @@ def get_ciba():
     return note_ch, note_en
 
 
-def send_message(to_user, access_token, region_name, weather, temp, wind_dir, note_ch, note_en):
-    url = "https://api.weixin.qq.com/cgi-bin/message/template/send?access_token={}".format(access_token)
+def send_message(to_user, access_token, region_name, weather, temp, wind_dir, air_humidity, air_quality, pm, ultraviolet_rays, note_ch, note_en, config):
+    url = f"https://api.weixin.qq.com/cgi-bin/message/template/send?access_token={access_token}"
     week_list = ["星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"]
     year = localtime().tm_year
     month = localtime().tm_mon
@@ -149,7 +168,7 @@ def send_message(to_user, access_token, region_name, weather, temp, wind_dir, no
         "topcolor": "#FF0000",
         "data": {
             "date": {
-                "value": "{} {}".format(today, week),
+                "value": f"{today} {week}",
                 "color": get_color()
             },
             "region": {
@@ -166,6 +185,22 @@ def send_message(to_user, access_token, region_name, weather, temp, wind_dir, no
             },
             "wind_dir": {
                 "value": wind_dir,
+                "color": get_color()
+            },
+            "air_humidity": {
+                "value": air_humidity,
+                "color": get_color()
+            },
+            "air_quality": {
+                "value": air_quality,
+                "color": get_color()
+            },
+            "pm": {
+                "value": pm,
+                "color": get_color()
+            },
+            "ultraviolet_rays": {
+                "value": ultraviolet_rays,
                 "color": get_color()
             },
             "love_day": {
@@ -186,9 +221,9 @@ def send_message(to_user, access_token, region_name, weather, temp, wind_dir, no
         # 获取距离下次生日的时间
         birth_day = get_birthday(value["birthday"], year, today)
         if birth_day == 0:
-            birthday_data = "今天{}生日哦，祝{}生日快乐！".format(value["name"], value["name"])
+            birthday_data = f"今天{value['name']}生日哦，祝{value['name']}生日快乐！"
         else:
-            birthday_data = "距离{}的生日还有{}天".format(value["name"], birth_day)
+            birthday_data = f"距离{value['name']}的生日还有{birth_day}天"
         # 将生日数据插入data
         data["data"][key] = {"value": birthday_data, "color": get_color()}
     headers = {
@@ -210,25 +245,14 @@ def send_message(to_user, access_token, region_name, weather, temp, wind_dir, no
 
 
 if __name__ == "__main__":
-    try:
-        with open("config.txt", encoding="utf-8") as f:
-            config = eval(f.read())
-    except FileNotFoundError:
-        print("推送消息失败，请检查config.txt文件是否与程序位于同一路径")
-        os.system("pause")
-        sys.exit(1)
-    except SyntaxError:
-        print("推送消息失败，请检查配置文件格式是否正确")
-        os.system("pause")
-        sys.exit(1)
-
+    config = get_config()
     # 获取accessToken
-    accessToken = get_access_token()
+    accessToken = get_access_token(config)
     # 接收的用户
     users = config["user"]
     # 传入地区获取天气信息
     region = config["region"]
-    weather, temp, wind_dir = get_weather(region)
+    weather, temp, wind_dir, air_humidity, air_quality, pm, ultraviolet_rays = get_weather(region, config)
     note_ch = config["note_ch"]
     note_en = config["note_en"]
     if note_ch == "" and note_en == "":
@@ -237,7 +261,7 @@ if __name__ == "__main__":
     # 公众号推送消息
     for user in users:
         if weather and temp and wind_dir:
-            send_message(user, accessToken, region, weather, temp, wind_dir, note_ch, note_en)
+            send_message(user, accessToken, region, weather, temp, wind_dir, air_humidity, air_quality, pm, ultraviolet_rays, note_ch, note_en, config)
         else:
             print("未获取到有效的天气信息，无法发送消息给用户:", user)
     os.system("pause")
